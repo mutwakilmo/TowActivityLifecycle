d8cc44da711932af3f8f8ef7485033d95bfd425c
mutwakilmo <mutwakilmo@gmail.com>
2020-02-10 17:45:35 +0100

    Initial commit

---
+++ app/src/main/java/com/mutwakilmo/android/towactivitylifecycle/launchActivity.java
@@ -0,0 +1,80 @@
+package com.mutwakilmo.android.towactivitylifecycle;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class launchActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY = "com.mutwakilmo.android.twoactivities.extra.REPLY";
+
+
+    public static final String LOG_TAG = launchActivity.class.getSimpleName();
+    private EditText mReply;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+
+
+    }
+
+    public void returnReply(View view) {
+        mReply = findViewById(R.id.editText_second);
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK,replyIntent);
+        finish();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+}
+
